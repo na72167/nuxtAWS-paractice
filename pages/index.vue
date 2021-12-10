71f1146dd1d2b9d92b6a929af6fd3e0319c0f82d
yh72167 <yh.19972372@gmail.com>
2021-12-11 07:37:39 +0900

    初期コミット

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>テスト</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class home extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
